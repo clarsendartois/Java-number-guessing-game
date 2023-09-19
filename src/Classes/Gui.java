7091a3643265c18c797eea9d53a710038c84dd97
Clarsen D'Artois <116375601+clarsendartois@users.noreply.github.com>
2023-09-18 20:54:32 -0400

    modified

--- src/Classes/Gui.java
+++ src/Classes/Gui.java
@@ -7,7 +7,10 @@ package Classes;
 import java.awt.Dimension;
 import java.awt.Rectangle;
 import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -27,6 +30,42 @@ public class Gui {// start
         frame.setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/Asset/logo.png")));
     }// end
 
-    
+    // allowing interger only when user enter his/her guessing number
+    public static void _intergerOnly(KeyEvent evt) {// start
+        // get the character
+        char _character = evt.getKeyChar();
+        // check user input
+        if (Character.isDigit(_character)
+                || _character == KeyEvent.VK_SPACE
+                || _character == KeyEvent.VK_DELETE
+                || _character == KeyEvent.VK_SLASH) {
+
+        } else {
+            evt.consume();
+            // creating beep notification sound
+            Toolkit.getDefaultToolkit().beep();
+
+        }
+
+    }// end
+
+    // Creating prompt message
+    public static void _message(String message, int errorTye) {// start
+
+        switch (errorTye) {
+            case 0:
+                JOptionPane.showMessageDialog(null, message, "ERROR", JOptionPane.ERROR_MESSAGE);
+                break;
+            case 1:
+                JOptionPane.showMessageDialog(null, message, "SUCCESS", JOptionPane.INFORMATION_MESSAGE);
+                break;
+            case 2:
+                JOptionPane.showMessageDialog(null, message, "WARNING", JOptionPane.WARNING_MESSAGE);
+                break;
+            default:
+                break;
+
+        }
+    }// end
 
 }// end class
