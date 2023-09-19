329e5a764a229a6b159c55febe923112024f221a
Clarsen D'Artois <116375601+clarsendartois@users.noreply.github.com>
2023-09-19 06:58:22 -0400

    modified

---
+++ src/Classes/GuessNumber.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+
+/**
+ *
+ * @author darto
+ */
+public class GuessNumber {// start
+
+    private static int _RANDOMGEN = 0;
+
+    // Create a random number
+    public static int _randomNumber() {// start
+        _RANDOMGEN = (int) Math.round(Math.random() * 100); // 1-100
+        return _RANDOMGEN;
+    }// ende
+
+    // Check number if 0, ex. 1-9, should be 01-09
+    private static int _checkIfZero(int num) {// start
+        int newNum = 0;
+        if (num == 0) {
+            newNum = _randomNumber();
+        }else {
+            newNum = num;
+        }
+        return newNum;
+
+    }// end
+
+}// end class
