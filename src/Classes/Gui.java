27722171b54e6f94dd9119438775578806d30e7a
Clarsen D'Artois <116375601+clarsendartois@users.noreply.github.com>
2023-10-04 20:56:15 -0400

    modified

--- src/Classes/Gui.java
+++ src/Classes/Gui.java
@@ -4,6 +4,9 @@
  */
 package Classes;
 
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
 import javax.swing.JFrame;
 
 /**
@@ -13,6 +16,15 @@ import javax.swing.JFrame;
 public class Gui {
     // Center JFRAME
     public static void _centerFrame(JFrame frame) {
-
+        Rectangle rec = frame.getBounds();
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setLocation((dim.width - rec.width) / 2, (dim.height - rec.height) / 2);
     }
+
+    // Assign icon to Frame
+    public void setIconFrame(JFrame frame) {
+        frame.setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/Asset/logo.png")));
+    }
+
+    
 }
